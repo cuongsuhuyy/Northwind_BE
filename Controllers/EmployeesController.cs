82d8c3362d51bdc3ef1e5891aff881028d69b31c
cuongsuhuyy <cuongsuhuy@gmail.com>
2021-05-25 01:29:12 +0700

    my first commit

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace NorthwindAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmployeesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from dbo.Employees";
+            DataTable table = new DataTable();
+            //string sqlDataSource = _configuration.GetConnectionString("NorthwindCon");
+            string sqlDataSource = "Data Source=.;Initial Catalog=Northwind;Integrated Security=True";
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }    
+            }    
+            return new JsonResult (table);
+        }
+    }
+}
